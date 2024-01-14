6a427b45cb2dc11a7a5ffdbab2a60d0c65155e80
youjunho613 <njn613@gmail.com>
2024-01-15 00:27:48 +0900

    supply page example table delete

--- components/table/SupplyTable.tsx
+++ components/table/SupplyTable.tsx
@@ -41,89 +41,15 @@ export default function SupplyTable() {
     { key: "deliveryPrice", label: "판매가" },
   ];
   return (
-    <>
-      <Table>
-        <TableHeader columns={columns}>
-          {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-        </TableHeader>
-        <TableBody items={data}>
-          {(item) => (
-            <TableRow key={item.id}>{(columnsKey) => <TableCell>{getKeyValue(item, columnsKey)}</TableCell>}</TableRow>
-          )}
-        </TableBody>
-      </Table>
-      <table className="supply-table">
-        <caption>입고 관리</caption>
-        <thead>
-          <tr>
-            <th scope="col">입고일자</th>
-            <th scope="col">브랜드</th>
-            <th scope="col">브랜드 코드</th>
-            <th scope="col">담당자</th>
-            <th scope="col">담당자 사번</th>
-            <th scope="col">제품 바코드</th>
-            <th scope="col">입고 수량</th>
-            <th scope="col">입고가</th>
-            <th scope="col">판매가</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th aria-label="입고일자" scope="row">
-              2024/01/02
-            </th>
-            <td aria-label="브랜드">부코스키</td>
-            <td aria-label="브랜드 코드">bukowski</td>
-            <td aria-label="담당자">홍길동</td>
-            <td aria-label="담당자 사번">0125</td>
-            <td aria-label="제품 바코드">123456789012</td>
-            <td aria-label="입고 수량">20</td>
-            <td aria-label="입고가">45000</td>
-            <td aria-label="판매가">60000</td>
-          </tr>
-          <tr>
-            <th scope="row">2024/01/02</th>
-            <td>부코스키</td>
-            <td>bukowski</td>
-            <td>홍길동</td>
-            <td>0125</td>
-            <td>123456789012</td>
-            <td>20</td>
-            <td>50000</td>
-            <td>60000</td>
-          </tr>
-          <tr>
-            <th scope="row">2024/01/02</th>
-            <td>부코스키</td>
-            <td>bukowski</td>
-            <td>홍길동</td>
-            <td>0125</td>
-            <td>123456789012</td>
-            <td>20</td>
-            <td>30000</td>
-            <td>40000</td>
-          </tr>
-          <tr>
-            <th scope="row">2024/01/02</th>
-            <td>부코스키</td>
-            <td>bukowski</td>
-            <td>홍길동</td>
-            <td>0125</td>
-            <td>123456789012</td>
-            <td>30</td>
-            <td>45000</td>
-            <td>60000</td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <th scope="row" colSpan={2}>
-              Total albums
-            </th>
-            <td colSpan={2}>77</td>
-          </tr>
-        </tfoot>
-      </table>
-    </>
+    <Table>
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={data}>
+        {(item) => (
+          <TableRow key={item.id}>{(columnsKey) => <TableCell>{getKeyValue(item, columnsKey)}</TableCell>}</TableRow>
+        )}
+      </TableBody>
+    </Table>
   );
 }
